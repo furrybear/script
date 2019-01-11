ca19f6e27dc6589099e164750a103df67eb17798
Furrybear <fbcll@outlook.com>
2019-01-11 16:21:17 +0800

    Some change.

--- docker-ubuntu-install.sh
+++ docker-ubuntu-install.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
-# step 1: 安装必要的一些系统工具
+#-----------------
+#备注：在Ubuntu18.04上可以工作，在Ubuntu18.10上不能工作
+#-----------------
+# 安装必要的一些系统工具
 sudo apt update
 sudo apt -y install apt-transport-https ca-certificates curl software-properties-common
-# step 2: 安装GPG证书
+# 安装GPG证书
 curl -fsSL http://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | sudo apt-key add -
-# Step 3: 写入软件源信息
+# 写入软件源信息
 sudo add-apt-repository "deb [arch=amd64] http://mirrors.aliyun.com/docker-ce/linux/ubuntu $(lsb_release -cs) stable"
-# Step 4: 更新并安装 Docker-CE
+# 更新并安装 Docker-CE
 sudo apt -y update
 sudo apt -y install docker-ce
-# Step 5 ：当前用户加入docker组
+# 当前用户加入docker组
 sudo usermod -a -G docker $USER
