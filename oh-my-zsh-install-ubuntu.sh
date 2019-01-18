fee47787daa5e9714af9c89e54b7728bf60cfb02
Furrybear <fbcll@outlook.com>
2019-01-18 15:57:12 +0800

    Some change.

--- oh-my-zsh-install-ubuntu.sh
+++ oh-my-zsh-install-ubuntu.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+sudo apt install curl
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 sed -i "s/ZSH_THEME=\"robbyrussell\"/ZSH_THEME=\"ys\"/g" ~/.zshrc
-unsetopt AUTO_CD
+#zsh -c "unsetopt AUTO_CD"
---
+++ set-git-username-and-email.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+git config --global user.name "Furrybear"
+git config --global user.email "fbcll@outlook.com"
