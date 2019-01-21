7f663b5555b1288ca91e17d842c532f6a7e7f6e9
Furrybear <fbcll@outlook.com>
2019-01-21 15:35:40 +0800

    Some change.

--- oh-my-zsh-install-ubuntu.sh
+++ oh-my-zsh-install-ubuntu.sh
@@ -1,5 +1,57 @@
 #!/bin/sh
+
+main() {
+  
+  set -e
+
+  if ! command -v zsh >/dev/null 2>&1; then
+    printf "Zsh is not installed! Please install zsh first!\n"
+    exit
+  fi
+
+  if [ ! -n "$ZSH" ]; then
+    ZSH=~/.oh-my-zsh
+  fi
+
+  if [ -d "$ZSH" ]; then
+    printf "You already have Oh My Zsh installed.\n"
+    printf "You'll need to remove $ZSH if you want to re-install.\n"
+    exit
+  fi
+
+  # Prevent the cloned repository from having insecure permissions. Failing to do
+  # so causes compinit() calls to fail with "command not found: compdef" errors
+  # for users with insecure umasks (e.g., "002", allowing group writability). Note
+  # that this will be ignored under Cygwin by default, as Windows ACLs take
+  # precedence over umasks except for filesystems mounted with option "noacl".
+  umask g-w,o-w
+
+  printf "Cloning Oh My Zsh...\n"
+  command -v git >/dev/null 2>&1 || {
+    echo "Error: git is not installed"
+    exit 1
+  }
+  env git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git "$ZSH" || {
+    printf "Error: git clone of oh-my-zsh repo failed\n"
+    exit 1
+  }
+
+
+  printf "Looking for an existing zsh config...\n"
+  if [ -f ~/.zshrc ] || [ -h ~/.zshrc ]; then
+    printf "Found ~/.zshrc. Backing up to ~/.zshrc.pre-oh-my-zsh\n";
+    mv ~/.zshrc ~/.zshrc.pre-oh-my-zsh;
+  fi
+
+  printf "Using the Oh My Zsh template file and adding it to ~/.zshrc\n"
+  cp "$ZSH"/templates/zshrc.zsh-template ~/.zshrc
+  sed "/^export ZSH=/ c\\
+  export ZSH=\"$ZSH\"
+  " ~/.zshrc > ~/.zshrc-omztemp
+  mv -f ~/.zshrc-omztemp ~/.zshrc
+
+}
+
 sudo apt install curl
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+main
 sed -i "s/ZSH_THEME=\"robbyrussell\"/ZSH_THEME=\"ys\"/g" ~/.zshrc
-#zsh -c "unsetopt AUTO_CD"
